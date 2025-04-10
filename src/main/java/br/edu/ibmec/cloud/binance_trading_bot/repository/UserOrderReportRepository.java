fc1695dae3e08a18f481e2a917dccf6e6c4be3fb
César Viana Barros <126491073+CVbarro@users.noreply.github.com>
2025-04-10 20:47:47 -0300

    Codigo consertado

--- src/main/java/br/edu/ibmec/cloud/binance_trading_bot/repository/UserOrderReportRepository.java
+++ src/main/java/br/edu/ibmec/cloud/binance_trading_bot/repository/UserOrderReportRepository.java
@@ -1,6 +1,6 @@
 package br.edu.ibmec.cloud.binance_trading_bot.repository;
 
-import br.edu.ibmec.cloud.binance_trading_bot.models.UserOrderReport;
+import br.edu.ibmec.cloud.binance_trading_bot.model.UserOrderReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
